cf95c014a62611ae51297a8daf5fa7e8390d8867
Simon Holloway <simon@syhol.io>
2021-05-29 23:53:16 +0100

    'chore: tidyup to callables'

--- src/module.ts
+++ src/module.ts
@@ -6,9 +6,9 @@ import {
   ServiceLocator,
   Shutdown,
   AbstractClass,
-  AllInstanceType,
-  Method,
-  Newable,
+  callableSetter,
+  Callable,
+  EventSink,
 } from '@badbury/ioc';
 import { HttpRoute } from './http-route';
 import { HttpServer } from './http-server';
@@ -17,25 +17,19 @@ export class StartHttpServer {
   constructor(public readonly port: number) {}
 }
 
-export abstract class HttpRouteDefinition<I, O> implements Definition<HttpRouteDefinition<I, O>> {
+export class HttpRouteDefinition<I, O> implements Definition<HttpRouteDefinition<I, O>> {
   definition = HttpRouteDefinition;
-  constructor(public readonly route: { prototype: HttpRoute<I, O> }) {}
-  abstract register(resolver: ServiceLocator, server: HttpServer): void;
+
+  constructor(
+    public readonly route: { prototype: HttpRoute<I, O> },
+    private readonly callable: Callable<[I], [], O>,
+  ) {}
+
+  register(resolver: ServiceLocator, sink: EventSink, server: HttpServer): void {
+    const route = resolver.get(this.route);
+    server.use(route, (req: I) => this.callable.handle([req], resolver, sink));
+  }
 }
-
-type MethodOf<
-  TClassType extends Newable,
-  TSubjectType,
-  TExtrasType extends AbstractClass[],
-  TReturnType
-> = Method<InstanceType<TClassType>, [TSubjectType, ...AllInstanceType<TExtrasType>], TReturnType>;
-
-type FunctionOf<
-  TSubject,
-  TExtrasType extends AbstractClass[],
-  TReturn = void,
-  TExtras extends any[] = AllInstanceType<TExtrasType>
-> = (subject: TSubject, ...args: TExtras) => TReturn;
 
 export class HttpRouteDefinitionBuilder<I, O, A extends AbstractClass[] = []> {
   constructor(
@@ -47,63 +41,11 @@ export class HttpRouteDefinitionBuilder<I, O, A extends AbstractClass[] = []> {
     return new HttpRouteDefinitionBuilder(this.route, args);
   }
 
-  do(target: FunctionOf<I, A, O>): HttpRouteDefinition<I, O>;
-  do<C extends Newable, M extends MethodOf<C, I, A, O>>(
-    target: C,
-    method: M,
-  ): HttpRouteDefinition<I, O>;
-  do<C extends Newable, M extends MethodOf<C, I, A, O>>(
-    target: C | FunctionOf<I, A, O>,
-    method?: M,
-  ): HttpRouteDefinition<I, O> {
-    return method
-      ? new ClassHttpRouteDefinition(this.route, this.args, target as C, method)
-      : new FunctionHttpRouteDefinition(this.route, this.args, target as FunctionOf<I, A, O>);
-  }
-}
-
-export class ClassHttpRouteDefinition<
-  I,
-  O,
-  A extends AbstractClass[],
-  V extends Newable,
-  M extends MethodOf<V, I, A, O>
-> extends HttpRouteDefinition<I, O> {
-  constructor(
-    public readonly route: { prototype: HttpRoute<I, O> },
-    public args: A,
-    private listenerClass: V,
-    private listenerMethod: M,
-  ) {
-    super(route);
-  }
-
-  register(resolver: ServiceLocator, server: HttpServer): void {
-    const args = this.args.map((key) => resolver.get(key)) as AllInstanceType<A>;
-    const route = resolver.get(this.route);
-    const handler = resolver.get(this.listenerClass);
-    server.use(route, (req: I) => handler[this.listenerMethod](req, ...args));
-  }
-}
-
-export class FunctionHttpRouteDefinition<
-  I,
-  O,
-  A extends AbstractClass[]
-> extends HttpRouteDefinition<I, O> {
-  constructor(
-    public readonly route: { prototype: HttpRoute<I, O> },
-    public args: A,
-    private handler: FunctionOf<I, A, O>,
-  ) {
-    super(route);
-  }
-
-  register(resolver: ServiceLocator, server: HttpServer): void {
-    const args = this.args.map((key) => resolver.get(key)) as AllInstanceType<A>;
-    const route = resolver.get(this.route);
-    server.use(route, (req: I) => this.handler(req, ...args));
-  }
+  do = callableSetter()
+    .withPassedArgs<[I]>()
+    .withContainerArgs(this.args)
+    .withReturn<O>()
+    .map((callable) => new HttpRouteDefinition(this.route, callable));
 }
 
 export class HttpModule {
@@ -115,7 +57,7 @@ export class HttpModule {
         .do((event, server) => {
           for (const definition of event.definitions) {
             if (definition instanceof HttpRouteDefinition) {
-              definition.register(event.container, server);
+              definition.register(event.container, event.container, server);
             }
           }
         }),
